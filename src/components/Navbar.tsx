b06cd7a95f916f341dcdce67c0d4a51ced9e8102
Pawarasasmina <pawarasasmina2002@icloud.com>
2025-01-17 20:13:08 +0530

    6666

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Menu, X } from "lucide-react";
-import logo from "../assets/companylogo.png"; // Import logo
+import logo from "../assets/logoo.png"; // Import logo
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
